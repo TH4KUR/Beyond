7573788b370096402210b7026a36823b539858b1
TH4KUR <max910payne@gmail.com>
2021-08-31 14:29:08 +0530

    Initial Commit 🚀🚀

---
+++ js/autoplayer.js
@@ -0,0 +1,21 @@
+"use-strict";
+
+class Autoplay {
+  constructor(container) {
+    this.container = container;
+  }
+  attachListener() {
+    this.container.addEventListener("mouseenter", () => {
+      console.log(Array.from(this.container.children));
+      let [video] = Array.from(this.container.children);
+      video.play();
+    });
+    this.container.addEventListener("mouseleave", () => {
+      let [video] = Array.from(this.container.children);
+      video.pause();
+    });
+  }
+}
+
+let valerian = new Autoplay(document.querySelector(".features__img-2"));
+valerian.attachListener();
