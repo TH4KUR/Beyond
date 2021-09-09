699af0b84d042de6fee90bf268c1b0a3ac21609d
TH4KUR <max910payne@gmail.com>
2021-09-09 21:27:07 +0530

    Some more...

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -4,13 +4,11 @@ let btnArr = [
   document.querySelector(".navigation__link-1"),
   document.querySelector(".navigation__link-2"),
 ];
-
 btnArr.forEach((btn) =>
   btn.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log(this, e);
-    document
-      .querySelector(`.${e.target.href.split(".")[1]}`)
-      .scrollIntoView({ behavior: "smooth" });
+    let zx = e.target.dataset.to;
+    console.log(zx, document.querySelector(zx));
+    document.querySelector(zx).scrollIntoView({ behavior: "smooth" });
   })
 );
